3521d37829e8818f60451427076a5a1acf184b9e
Toni Espadas <55438709+anthonyswords@users.noreply.github.com>
2022-11-10 20:25:34 +0100

    Update bet_house_2023.py
    # afegir columna del dia que hem exportat la taula --fet
    # documentar // definir funcions pertinents. --fet
    # orientar codi a POO

--- source/module/bet_house_2023.py
+++ source/module/bet_house_2023.py
@@ -1,30 +1,49 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from typing import List
+from datetime import datetime
 
+class WebScrapperBetHouse23():
 
-url = 'https://eurovisionworld.com/odds/eurovision'
-
-header = {
-  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
-  "X-Requested-With": "XMLHttpRequest"
-}
-
-r = requests.get(url, headers=header)
-status_code = r.status_code
-
-if status_code == 200:
-
-    # We pass the HTML content of the web to a BeautifulSoup() object
-    html = BeautifulSoup(r.text, "html.parser")
-    dfs = pd.read_html(r.text)
-
-    # We get all the divs where the inputs are
-    entradas = html.find_all('div', {'class': 'poll_inner'})
-
-else:
-    print("Status Code %d" % status_code)
+    def __init__(self):
+        self.start_time = datetime.now()
+        self.url = "https://eurovisionworld.com/odds/eurovision"
+        self.options = webdriver.FirefoxOptions()
+        self.options.headless = True
+        self.header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+          "X-Requested-With": "XMLHttpRequest"
+        }
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(),
+                                        options=self.options).get(self.url)
     
-df = dfs[2]
-df.drop(['Unnamed: 0'], axis=1, inplace=True)
-df.to_csv('OddsRatioEurovision2023')
+    def get_request(self) -> requests:
+        return requests.get(self.url, headers=self.header)
+    
+    def get_df_BetHouse23(self, pos: int=2) -> pd.DataFrame:
+        """
+        Returns a DataFrame according to the main table about 
+        odds/winrates from url.
+        Args:
+            pos (int, optional): pos-ition 2 by default
+        Return:
+            Dataframe
+        """
+        status_code = self.get_request().status_code
+        if status_code == 200:
+            # We pass the HTML content of the web to a BeautifulSoup() object
+            html = BeautifulSoup(r.text, "html.parser")
+            try:
+                df = pd.read_html(r.text)[pos]
+                df.drop(['Unnamed: 0'], axis=1, inplace=True)
+                df.rename(columns={'Unnamed: 1': 'country', 
+                                   'winningchance': 'winrate'},
+                          inplace=True)
+                df.loc[:,'scrapped_day'] = datetime.now()
+                df.head()
+                return df
+            except Exception as e:
+                print(e)       
+        else:
+            print("Status Code %d" % status_code)
